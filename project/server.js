2d90430a16eaa593a2dae9774d06fe10d80e814e
dspsilva27 <dspcdewnith@gmail.com>
2026-06-28 12:01:11 +0530

    S2: hello-world app

--- project/server.js
+++ project/server.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    session: "NB60007CEM S2"
+  });
 });
 
-app.listen(3000, () => {
-    console.log('Open http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
