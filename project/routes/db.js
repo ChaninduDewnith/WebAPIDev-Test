385eb19dc10c4645f05ee931c4911c677846aad9
dspsilva27 <dspcdewnith@gmail.com>
2026-07-21 22:52:11 +0530

    Update MongoDB connection string to use SRV format

--- project/routes/db.js
+++ project/routes/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 async function connectDB() {
-    const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}?ssl=true&replicaSet=${process.env.MONGO_REPLICA_SET}&authSource=admin&retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.i8joo4s.mongodb.net/`;
     await mongoose.connect(uri);
     return mongoose.connection.db;
 }
